46f590377f438fd83e3354775611181791eca8cd
John Holbrook <johndholbrook@hotmail.com>
2012-05-28 20:10:06 -0600

    Finish sign in

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -17,4 +17,8 @@ module SessionsHelper
     @current_user ||= User.find_by_remember_token(cookies[:remember_token])
   end
 
+  def sign_out
+    current_user = nil
+    cookies.delete(:remember_token)
+  end
 end
